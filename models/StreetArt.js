145e2f490b788340ab1a41e8964c765aa2122b33
LenySebski <sebastianlenart@gmail.com>
2022-01-28 04:54:15 +0000

    server, router, model initial config

--- models/StreetArt.js
+++ models/StreetArt.js
@@ -1,12 +1,58 @@
 const mongoose = require('mongoose')
-
+const Schema = mongoose.Schema
 //mogoose schema for the data
-const StreetartSchema = new mongoose.Schema({
+const streetartSchema = new Schema({
+  
     title: {
-        type: String,
-        required: true,
-    },
-});
+        type: String
+        },
+    artist: {
+            type:String
+        }, 
+    year: {type:Number
+        },
+    active: {type:Boolean
+        },
+    image: {type: String,
+            required:true
+        },
+    thumbnail: {type:String
+        },
+    location: {
+            zip:{type:Number,
+                required:true,
+                default:101
+            },
+            street:{type: String
+            },
+            coordinates:{
+                longitude:{type:Number
+                },
+                latitude:{type:Number
+                } 
+            }
+        },
 
-const Streetart = mongoose.model("Streetart", StreetartSchema);
-module.exports = Streetart;
+    social: {
+            originalPoster:{type:String
+            },
+            likes:{type:Number
+            },
+            comments:{type:Array
+            }
+        },
+
+        
+    dateCreate:{type:Date
+            },
+    dateEdit:{type:Date
+            }
+        
+        
+    
+}
+
+
+);
+
+module.exports = mongoose.model("Streetart", streetartSchema);
